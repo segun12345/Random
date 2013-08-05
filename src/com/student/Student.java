5b4306ad374b81e0759f55a3aee7577d67abbdd2
segun12345 <segun.runsewe@gmail.com>
2013-08-05 23:48:41 +0100

    adding Student File

---
+++ src/com/student/Student.java
@@ -0,0 +1,38 @@
+package com.student;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: graduate
+ * Date: 05/08/2013
+ * Time: 23:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class Student {
+    private String name;
+    private String address;
+    private String postcode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+}
