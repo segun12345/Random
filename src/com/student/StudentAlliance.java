dad3b176f361012da3968392b1036427e39ec5b0
segun12345 <segun.runsewe@gmail.com>
2013-08-07 00:55:19 +0100

    Student Alliance

--- src/com/student/StudentAlliance.java
+++ src/com/student/StudentAlliance.java
@@ -9,6 +9,6 @@ package com.student;
  */
 public class StudentAlliance {
     public static void main(String... args){
-        System.out.println("Hello");
+        System.out.println("Hello");  .>>
     }
 }
