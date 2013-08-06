d8f39191f2de87922480985459fda3caa8767313
segun12345 <segun.runsewe@gmail.com>
2013-08-07 00:53:45 +0100

    Student Alliance

---
+++ src/com/student/StudentAlliance.java
@@ -0,0 +1,14 @@
+package com.student;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: oru02
+ * Date: 07/08/2013
+ * Time: 00:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class StudentAlliance {
+    public static void main(String... args){
+        System.out.println("Hello");
+    }
+}
